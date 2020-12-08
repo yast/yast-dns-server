d1d8f35c26c6c3d7dd71cb4f7f7fc02e8ab519ed
Ladislav Slezák <lslezak@suse.cz>
2020-12-08 16:23:07 +0100

    Added GitHub Action

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,21 +23,37 @@ ENV["Y2DIR"] = SRC_PATH
 require "yast"
 require "yast/rspec"
 
+RSpec.configure do |config|
+  config.mock_with :rspec do |c|
+    # make sure we mock only the existing methods
+    # https://relishapp.com/rspec/rspec-mocks/v/3-0/docs/verifying-doubles/partial-doubles
+    c.verify_partial_doubles = true
+  end
+end
+
 if ENV["COVERAGE"]
   require "simplecov"
   SimpleCov.start do
     add_filter "/test/"
   end
 
-  # for coverage we need to load all ruby files
-  Dir["#{SRC_PATH}/{lib,modules}/**/*.rb"].each { |f| require_relative f }
+  # track all ruby files under src
+  SimpleCov.track_files("#{SRC_PATH}/**/*.rb")
 
-  # use coveralls for on-line code coverage reporting at Travis CI
-  if ENV["TRAVIS"]
-    require "coveralls"
+  # additionally use the LCOV format for on-line code coverage reporting at CI
+  if ENV["CI"] || ENV["COVERAGE_LCOV"]
+    require "simplecov-lcov"
+
+    SimpleCov::Formatter::LcovFormatter.config do |c|
+      c.report_with_single_file = true
+      # this is the default Coveralls GitHub Action location
+      # https://github.com/marketplace/actions/coveralls-github-action
+      c.single_report_path = "coverage/lcov.info"
+    end
+
     SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
       SimpleCov::Formatter::HTMLFormatter,
-      Coveralls::SimpleCov::Formatter
+      SimpleCov::Formatter::LcovFormatter
     ]
   end
 end
